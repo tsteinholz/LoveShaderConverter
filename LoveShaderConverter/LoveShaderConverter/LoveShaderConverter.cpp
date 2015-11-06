ef4f1572b466ad50afa49eab965d03f4bc928fce
Thomas Steinholz <tsteinholz@yahoo.com>
2015-11-06 16:54:58 -0500

    this is bad lol

--- LoveShaderConverter/LoveShaderConverter/LoveShaderConverter.cpp
+++ LoveShaderConverter/LoveShaderConverter/LoveShaderConverter.cpp
@@ -1,3 +1,8 @@
+// LoveShaderConverter.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
 /*/-----------------------------------------------------------------------------------------------------------------/*/
 /*/                                                                                                                 /*/
 /*/                                 ______________________________________                                          /*/
@@ -31,10 +36,6 @@
 /*/                                                                                                                 /*/
 /*/                                                                                                                 /*/
 /*/-----------------------------------------------------------------------------------------------------------------/*/
-
-// LoveShaderConverter.cpp : Defines the entry point for the console application.
-
-#include "stdafx.h"
 
 #include <boost/algorithm/string/replace.hpp>
 
