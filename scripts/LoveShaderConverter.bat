a9968ce6ec40e307a8d457a6ce653bb41a6316d2
Thomas Steinholz <tsteinholz@yahoo.com>
2015-11-06 19:41:23 -0500

    Update LoveShaderConverter.bat

--- scripts/LoveShaderConverter.bat
+++ scripts/LoveShaderConverter.bat
@@ -1,4 +1,7 @@
 @echo off
+echo Welcome to Love Shader Converter,
+echo this file needs to be executed from the same directory as the EXE.
+echo
 set /p file="Please enter the shader file:"
 LoveShaderConverter.exe %file%
 pause
