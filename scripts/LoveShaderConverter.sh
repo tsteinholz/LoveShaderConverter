9b8c2e384a1a4d958e7afc049eabe0a278f9c1e2
Thomas Steinholz <tsteinholz@yahoo.com>
2015-11-07 02:47:15 -0500

    Update LoveShaderConverter.sh

--- scripts/LoveShaderConverter.sh
+++ scripts/LoveShaderConverter.sh
@@ -1,2 +1,7 @@
 #!bin/bash
+echo Welcome to Love Shader Converter,
+echo this file needs to be executed from the same directory as the EXE.
 echo
+echo Please enter the shader file:
+read file_name
+./LoveShaderConverter $file_name
